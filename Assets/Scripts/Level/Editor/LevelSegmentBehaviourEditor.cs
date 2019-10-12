51ddfdc1b1f00371aa330ec23e7c834bd0eab2d9
Guillermo I <guillermo.iruretagoyena@gmail.com>
2019-10-12 09:33:26 +0200

    Adds definition of a LevelSegment which has transform to express a bezier line. Adds an editor script to draw the bezier of a LevelSegment with handles. Adds a static function to create segments depending on the type.

---
+++ Assets/Scripts/Level/Editor/LevelSegmentBehaviourEditor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LevelSegmentBehaviour))]
+public class LevelSegmentBehaviourEditor : Editor
+{
+    private void OnSceneViewGUI(SceneView sv)
+    {
+        LevelSegmentBehaviour segment = target as LevelSegmentBehaviour;
+
+        segment.startPoint.position = Handles.PositionHandle(segment.startPoint.position, Quaternion.identity);
+        segment.endPoint.position = Handles.PositionHandle(segment.endPoint.position, Quaternion.identity);
+        segment.startTangent.position = Handles.PositionHandle(segment.startTangent.position, Quaternion.identity);
+        segment.endTangent.position = Handles.PositionHandle(segment.endTangent.position, Quaternion.identity);
+    }
+
+    [DrawGizmo(GizmoType.InSelectionHierarchy | GizmoType.NotInSelectionHierarchy)]
+    static void DrawHandles(LevelSegmentBehaviour segment, GizmoType gizmoType)
+    {
+        GUIStyle style = new GUIStyle();
+        style.normal.textColor = Color.blue;
+        Handles.Label(segment.startPoint.position, segment.name, style);
+        Handles.DrawBezier(segment.startPoint.position, segment.endPoint.position, segment.startTangent.position, segment.endTangent.position, Color.red, null, 2f);
+    }
+
+    void OnEnable()
+    {
+        SceneView.duringSceneGui += OnSceneViewGUI;
+    }
+
+    void OnDisable()
+    {
+        SceneView.duringSceneGui -= OnSceneViewGUI;
+    }
+}
---
+++ Assets/Scripts/Level/LevelSegmentBehaviour.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class LevelSegmentBehaviour : MonoBehaviour
+{
+    public Transform startPoint;
+    public Transform startTangent;
+
+    public Transform endPoint;
+    public Transform endTangent;
+
+    public LevelSegmentBehaviour previousSegment;
+
+    [System.Serializable]
+    public enum SegmentType
+    {
+        //Uses startPoint/endPoint as start/end
+        start,
+        //Uses previousSegment as start/end
+        inner
+    }
+
+    protected SegmentType segmentType = SegmentType.inner;
+
+    public SegmentType GetSegmentType()
+    {
+        return segmentType;
+    }
+
+    /// <summary>
+    /// Create a Segment with its helper transform taking into account the type of the segment
+    /// </summary>
+    /// <param name="segmentType"></param>
+    /// <returns></returns>
+    public static LevelSegmentBehaviour CreateSegment(SegmentType segmentType)
+    {
+        GameObject segmentGO = new GameObject("Segment");
+        segmentGO.SetActive(false);
+
+        LevelSegmentBehaviour segment = segmentGO.AddComponent<LevelSegmentBehaviour>();
+
+        segment.endPoint = new GameObject("EndPoint").transform;
+        segment.endPoint.SetParent(segment.transform);
+        segment.endPoint.localPosition = Vector3.forward * 2;
+        segment.endPoint.localRotation = Quaternion.identity;
+
+        segment.endTangent = new GameObject("EndTanget").transform;
+        segment.endTangent.SetParent(segment.endPoint);
+        segment.endTangent.localPosition = Vector3.zero;
+        segment.endTangent.localRotation = Quaternion.identity;
+
+        if (segmentType == SegmentType.start)
+        {
+            segment.startPoint = new GameObject("StartPoint").transform;
+            segment.startPoint.SetParent(segment.transform);
+            segment.startPoint.localPosition = Vector3.zero;
+            segment.startPoint.localRotation = Quaternion.identity;
+
+            segment.startTangent = new GameObject("StartTangent").transform;
+            segment.startTangent.SetParent(segment.startPoint);
+            segment.startTangent.localPosition = Vector3.zero;
+            segment.startTangent.localRotation = Quaternion.identity;
+        }
+
+        segmentGO.SetActive(true);
+        return segment;
+    }
+}
