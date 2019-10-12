52b3e6e620d9df52430dfedf5b0e059f897ef7b5
Guillermo I <guillermo.iruretagoyena@gmail.com>
2019-10-12 13:10:26 +0200

    Starts defining GameRun, the execution of a particular game (player+level). Defines the score of a GameRun

---
+++ Assets/Scripts/GameRun/GameRun.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using UnityEngine;
+
+public class GameRun : MonoBehaviour
+{
+    public PlayerBehaviour playerPrefab;
+    public LevelBehaviour levelPrefab;
+  
+    protected PlayerBehaviour playerInstance;
+    protected LevelBehaviour levelInstance;
+
+    public GameScoreEvent OnGameScoreUpdate = new GameScoreEvent();
+    public GameScoreEvent OnGameEnded = new GameScoreEvent();
+
+    [System.Serializable]
+    public enum State
+    {
+        idle,
+        running
+    }
+
+    protected State currentState = State.idle;
+
+    //Current position of the player in composite bezier space
+    protected float currentT = 0.0f;
+
+    public void Prepare()
+    {
+        playerInstance = Instantiate(playerPrefab);
+        levelInstance = Instantiate(levelPrefab);
+    }
+
+    public void StartGame()
+    {
+        currentState = State.running;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentState)
+        {
+            case State.idle:
+                break;
+            case State.running:
+                UpdatePlayerPosition();
+                break;
+            default:
+                break;
+        } 
+    }
+
+    protected void UpdatePlayerPosition()
+    {
+        float desiredDisplacement = playerInstance.GetDisplacement();
+
+        Vector3 playerPosition = playerInstance.transform.position;
+        Quaternion playerRotation = playerInstance.transform.rotation;
+
+        levelInstance.MoveOnTheLevel(desiredDisplacement, ref currentT, ref playerPosition, ref playerRotation);
+
+        playerInstance.transform.position = playerPosition;
+        playerInstance.transform.rotation = playerRotation;
+    }
+}
---
+++ Assets/Scripts/GameRun/GameScore.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.Events;
+
+[System.Serializable]
+public class GameScore
+{
+    public int points = 0;
+    public bool finished = false;
+}
+
+[System.Serializable]
+public class GameScoreEvent : UnityEvent<GameScore> { }
