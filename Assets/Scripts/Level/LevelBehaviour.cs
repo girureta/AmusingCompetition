9257a89240a40a2112555c07303c3267fa0477aa
Guillermo I <guillermo.iruretagoyena@gmail.com>
2019-10-12 11:29:04 +0200

    Adds a function to evalue the retrieve the position on the level given T. Evalutes the composite bezier

--- Assets/Scripts/Level/LevelBehaviour.cs
+++ Assets/Scripts/Level/LevelBehaviour.cs
@@ -5,6 +5,31 @@ public class LevelBehaviour : MonoBehaviour
 {
     public LevelSegmentBehaviour[] segments = new LevelSegmentBehaviour[] { };
 
+    /// <summary>
+    /// How much each segment represent in the T of the Composite bezier formed by 'segments'
+    /// </summary>
+    protected float sizeSegmentT = 0.1f;
+
+    private void Awake()
+    {
+        sizeSegmentT = 1.0f / segments.Length;
+    }
+    public Vector3 EvaluateSegments(float t)
+    {
+        Vector3 result = Vector3.zero;
+
+        float tt = ((float)segments.Length) * t;
+        int segmentIndex = Mathf.FloorToInt(tt);
+
+        //Transfor composite T to segment's
+        float lowerT = sizeSegmentT * segmentIndex;
+        float localT = (t - lowerT) / sizeSegmentT;
+
+        result = segments[segmentIndex].EvaluateSegment(localT);
+
+        return result;
+    }
+
 #if  UNITY_EDITOR
     [ContextMenu("Add new segment")]
     protected void AddSegment()
