9a960aed0c4d5ebf4b4ffc1751be73f4ba71f554
Guillermo I <guillermo.iruretagoyena@gmail.com>
2019-10-12 10:50:52 +0200

    Defines the player with acceleration and max speed. Updates current speed on every update, given the acceleration and deltaTime Adds function to calculate the desired displacement given the current speed.

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public float acceleration = 0.1f;
+    public float maxSpeed = 1.0f;
+    protected float currentSpeed = 0.0f;
+
+
+    /// <summary>
+    /// How much the player should displace given its currentSpeed;
+    /// </summary>
+    /// <returns></returns>
+    public float GetDisplacement()
+    {
+        return currentSpeed * Time.deltaTime;
+    }
+
+    private void Update()
+    {
+        bool shouldAccelerate = IsTapping();
+        float accelerationSign = shouldAccelerate ? 1.0f : -1.0f;
+
+        float v = accelerationSign * acceleration * Time.deltaTime;
+        currentSpeed = currentSpeed + v;
+        currentSpeed = Mathf.Min(maxSpeed, currentSpeed);
+    }
+
+    protected bool IsTapping()
+    {
+        return true;
+    }
+}
