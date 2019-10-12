93242947d4a3c4973ab187288b915e883d7b993c
Guillermo I <guillermo.iruretagoyena@gmail.com>
2019-10-12 13:12:06 +0200

    Starts defining the main logic in AmusingCompetitionBehaviour. Starts defining the UI/view side of AmusingCompetitionBehaviour in AmusingCompetitionBehaviourUI.

---
+++ Assets/Scripts/Main/AmusingCompetitionBehaviour.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AmusingCompetitionBehaviour : MonoBehaviour
+{
+    public CameraBehaviour mainCamera;
+    public LevelSet levelSet;
+    public PlayerBehaviour selectedPlayerPrefab;
+    public int currentLevel = 0;
+
+    protected GameRun currentGameRun = null;
+
+    public UnityEvent OnGameStarted = new UnityEvent();
+    public GameScoreEvent OnGameEnded = new GameScoreEvent();
+    public GameScoreEvent OnShowEndScreen = new GameScoreEvent();
+    public UnityEvent OnShowAd = new UnityEvent();
+
+    protected enum State
+    {
+        uninitialized,
+        waitForTap,
+        playingGame,
+        showingEndScreen,
+        showingAd
+    }
+
+    protected State currentState = State.uninitialized;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PrepareLevel();
+    }
+
+    protected void PrepareLevel()
+    {
+        if (currentState != State.uninitialized)
+            return;
+
+        currentGameRun = new GameObject("GameRun").AddComponent<GameRun>();
+        currentGameRun.playerPrefab = selectedPlayerPrefab;
+        currentGameRun.levelPrefab = levelSet.levels[currentLevel];
+        currentGameRun.Prepare();
+        currentState = State.waitForTap;
+    }
+
+    public void StartLevel()
+    {
+        if (currentState == State.waitForTap)
+        {
+            currentGameRun.OnGameEnded.AddListener(OnGameFinished);
+            currentGameRun.StartGame();
+            OnGameStarted.Invoke();
+        }
+    }
+
+    protected void OnGameFinished(GameScore gameScore)
+    {
+        currentGameRun.OnGameEnded.RemoveListener(OnGameFinished);
+        currentState = State.showingEndScreen;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentState)
+        {
+            case State.waitForTap:
+                CheckTap();
+                break;
+        }
+    }
+
+    protected void CheckTap()
+    {
+        bool wasTapped = false;
+        wasTapped = Input.touchCount > 0;
+        wasTapped = wasTapped ||Input.anyKey;
+
+        if (wasTapped)
+        {
+            StartLevel();
+        }
+
+        }
+
+}
---
+++ Assets/Scripts/UI/AmusingCompetitionUIBehaviour.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmusingCompetitionUIBehaviour : MonoBehaviour
+{
+    public AmusingCompetitionBehaviour controller;
+    public TapPanel tapPanel;
+    public EndScreenPanel endScreenPanel;
+    public AdPanel adPanel;
+
+    private void Awake()
+    {
+        tapPanel.Show();
+        endScreenPanel.Hide();
+        adPanel.Hide();
+        controller.OnShowAd.AddListener(ShowAd);
+        controller.OnShowEndScreen.AddListener(OnShowEndScreen);
+        controller.OnGameStarted.AddListener(OnGameStarted);
+    }
+
+    public void StartLevel()
+    {
+        controller.StartLevel();
+    }
+
+    public void OnGameStarted()
+    {
+        tapPanel.Hide();
+    }
+
+    protected void ShowAd()
+    {
+        adPanel.Show();
+    }
+
+    protected void OnShowEndScreen(GameScore score)
+    {
+        endScreenPanel.Show(score);
+    }
+    
+}
+
+[System.Serializable]
+public class TapPanel
+{
+    public GameObject panel;
+
+    public void Show()
+    {
+        panel.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        panel.SetActive(false);
+    }
+}
+
+[System.Serializable]
+public class EndScreenPanel
+{
+    public GameObject panel;
+    public Text scoreText;
+
+    public void Show(GameScore score)
+    {
+        panel.SetActive(true);
+        scoreText.text = score.points.ToString();
+    }
+
+    protected void OnPlayAgainButton()
+    {
+        Hide();
+    }
+
+    public void Hide()
+    {
+        panel.SetActive(false);
+    }
+}
+
+[System.Serializable]
+public class AdPanel
+{
+    public GameObject panel;
+    public void Show()
+    {
+        //Retrieve ad somehow
+        //...
+        //
+        panel.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        //cleanup
+        panel.SetActive(false);
+    }
+}
